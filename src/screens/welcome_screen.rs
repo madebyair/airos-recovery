d0798454200acf4c9679c0a8efe4d11a37a09010
Oskar Nizioł <oskarniziol@gmail.com>
2024-03-16 16:06:03 +0100

    options in welcome screen

--- src/screens/welcome_screen.rs
+++ src/screens/welcome_screen.rs
@@ -1,4 +1,5 @@
 use console::style;
+use dialoguer::{theme::ColorfulTheme, Select};
 
 pub fn welcome_screen() {
     clearscreen::clear().expect("failed to clear screen");
@@ -9,6 +10,19 @@ pub fn welcome_screen() {
     println!("\n- You are at new computer, with disk from old.");
     println!("- Your computer UEFI has been restarted.");
     println!("- Your airos update was failed.");
-    println!("- You want to fix your airos erros {}", style("(requires error code)").black());
-    println!("- You are professional user, and you know what are you doing {}", style("(requires brain)").black());
+    println!("- You want to fix your airos erros {}", style("(requires error code)").white().italic());
+    println!("- You are professional user, and you know what are you doing {}", style("(requires brain)").white().italic());
+
+    println!("{}", style("\n * What should I do now?").bold());
+    println!("Now you need select one of the option shown below");
+
+    println!("{}", style("Use the up and down arrows, on your keyboard to navigate").white().bold());
+
+    let items = ["The system stopped loading and this is loading instead", "I'm nerd open, the doors!"];
+
+    let _selection = Select::with_theme(&ColorfulTheme::default())
+        .items(&items)
+        .default(0)
+        .interact()
+        .unwrap();
 }
