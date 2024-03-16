dfc96118b3f5d104298fc86ef9d4130e852621e9
Oskar Nizioł <oskarniziol@gmail.com>
2024-03-16 22:30:52 +0100

    add version to index

--- src/screens/index.rs
+++ src/screens/index.rs
@@ -3,6 +3,6 @@ use console::style;
 pub fn index() {
     clearscreen::clear().expect("failed to clear screen");
 
-    println!("{}", style("The Air Operating System Recovery").black().bold().on_white());
+    println!("{} {}", style("The Air Operating System Recovery").black().bold().on_white(), style(format!("(Version {})", env!("CARGO_PKG_VERSION"))).white().italic());
     println!("{}", style("Welcome to Recovery! What do you wanna do today?").bold());
 }
