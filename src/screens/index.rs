8436081bb66847ef087e686a5c018b2ada208184
Oskar Nizioł <oskarniziol@gmail.com>
2024-03-16 22:46:36 +0100

    index selection

--- src/screens/index.rs
+++ src/screens/index.rs
@@ -1,8 +1,20 @@
 use console::style;
+use dialoguer::Select;
+use dialoguer::theme::ColorfulTheme;
 
 pub fn index() {
     clearscreen::clear().expect("failed to clear screen");
 
     println!("{} {}", style("The Air Operating System Recovery").black().bold().on_white(), style(format!("(Version {})", env!("CARGO_PKG_VERSION"))).white().italic());
     println!("{}", style("Welcome to Recovery! What do you wanna do today?").bold());
+
+    println!("\n ✅  {} \n", style("Root mounted").green().bold());
+
+    let items = ["Mount root", "Reboot"];
+
+    let selection = Select::with_theme(&ColorfulTheme::default())
+        .items(&items)
+        .default(0)
+        .interact()
+        .unwrap();
 }
